9bf7414c4c03dfb788c762002313517e2d5ad34b
IgnacioGeo <101019155+IgnacioGeo@users.noreply.github.com>
2022-05-15 22:49:45 -0400

    Revert "Revert "Base Dato part2""
    This reverts commit 0d067e859815552c506283a21c7e5c96e30723ca.

---
+++ GameDemo/Assets/scripts/WebGet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class WebGet : MonoBehaviour
+{
+    public Text messageText;
+
+    readonly string getURLLevel = "http://localhost/GetBestDemo.php";//
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetLoadSave());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GetLoadSave()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(getURLLevel);
+
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+        }
+
+        else
+        {
+            messageText.text = www.downloadHandler.text;
+        }
+    }
+
+}
